88b2e100452ce50a7c3e2b6497ee515aa1c15ad9
zhongshaohan <zhongshaohan@bytedance.com>
2018-12-20 21:56:15 +0800

    fix: mq config

--- src/main/java/com/shzhong/mqorder/component/CancelOrderSender.java
+++ src/main/java/com/shzhong/mqorder/component/CancelOrderSender.java
@@ -2,10 +2,7 @@ package com.shzhong.mqorder.component;
 
 import com.shzhong.mqorder.domain.QueueEnum;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,13 +18,10 @@ public class CancelOrderSender {
 
     public void sendMessage(Long orderId, final long delayTimes) {
         //给延迟队列发送消息
-        amqpTemplate.convertAndSend(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getExchange(), QueueEnum.QUEUE_TTL_ORDER_CANCEL.getRouteKey(), orderId, new MessagePostProcessor() {
-            @Override
-            public Message postProcessMessage(Message message) throws AmqpException {
-                //给消息设置延迟毫秒值
-                message.getMessageProperties().setExpiration(String.valueOf(delayTimes));
-                return message;
-            }
+        amqpTemplate.convertAndSend(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getExchange(), QueueEnum.QUEUE_TTL_ORDER_CANCEL.getRouteKey(), orderId, message -> {
+            //给消息设置延迟毫秒值
+            message.getMessageProperties().setExpiration(String.valueOf(delayTimes));
+            return message;
         });
         log.info("send orderId:{}", orderId);
     }
